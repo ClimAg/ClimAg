75f74d56f69a44809cd0692937c06884e19378f4
Nithiya Streethran <nmstreethran@gmail.com>
2022-09-23 11:41:30 +0100

    formatting

--- climag/modvege/lib_read_output_files.py
+++ climag/modvege/lib_read_output_files.py
@@ -1,24 +1,30 @@
+"""lib_read_output_files.py
+
+https://github.com/YannChemin/modvege
+
+This is used only for development, to remove for operational mode
+"""
+
 import numpy as np
 
-# This is used only for development, to remove for operational mode
+out_csv = "out_cut.csv"
 
-##############################################
-# Definition of columns in out_cut.csv
-##############################################
-# day
-# Mean biomass                      (kg DM/ha)
-# Mean green vegetative biomass     (kg DM/ha)
-# Mean green reproductive biomass   (kg DM/ha)
-# Mean dry vegetative biomass       (kg DM/ha)
-# Mean dry reproductive biomass     (kg DM/ha)
-# Harvested Biomass                 (kg DM/ha)
-# Ingested Biomass                  (kg DM/ha)
-# Mean GRO biomass                  (kg DM/ha)
-# Mean available biomass for cut    (kg DM/ha)
 
-out_csv='out_cut.csv'
+def read_out(filename=out_csv):
+    """Read the "out_cut.csv" file
 
-def read_out(out_csv):
-    arr = np.genfromtxt(out_csv, delimiter=",", skip_header=0, names=True)
-    return(arr)
-
+    Definition of columns in out_cut.csv
+    ------------------------------------
+    day
+    Mean biomass                      (kg DM/ha)
+    Mean green vegetative biomass     (kg DM/ha)
+    Mean green reproductive biomass   (kg DM/ha)
+    Mean dry vegetative biomass       (kg DM/ha)
+    Mean dry reproductive biomass     (kg DM/ha)
+    Harvested Biomass                 (kg DM/ha)
+    Ingested Biomass                  (kg DM/ha)
+    Mean GRO biomass                  (kg DM/ha)
+    Mean available biomass for cut    (kg DM/ha)
+    """
+    arr = np.genfromtxt(filename, delimiter=",", skip_header=0, names=True)
+    return arr
